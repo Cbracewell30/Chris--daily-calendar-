7f54dcfd78ec53774dacfcf4c20da3bcb60f0f67
Chris <chrisbracewell35@gmail.com>
2022-01-09 12:23:38 -0500

    saving all recent changes

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,6 +1,9 @@
 var currentDte = moment().format('LL');
 var date = document.getElementById("currentDay");
 
+$(".saveBtn").hover( function() {
+  $(`#save-btn-${i}`).addClass("i:hover");
+});
 
 // Adding Current day to the page
   $("#currentDay").text(currentDte);
@@ -14,16 +17,23 @@ localStorage.setItem(timeBlock,userInput);
 
 
 var currentHour = moment().hours()
-for(let i = 0; i <= 17; i++){
+
+for(let i = 0; i <= 17; i++) {
   var plan = localStorage.getItem(i)
   $(`txt-input-${i}`).val(plan)
 
-// a
   if(i< currentHour){
   $(`#txt-input-${i}`).addClass("past");
   }
- 
-}
-else if (i===currentHour){
 
-}
+  else if (i===currentHour ) {
+    $(`#txt-input-${i}`).addClass("present");
+  }
+
+  else {
+    $(`#txt-input-${i}`).addClass("future");
+  }
+};
+
+// var inputLocal = localStorage.getItem(timeBlock,userInput);
+// document.getElementById(timeBlock);
